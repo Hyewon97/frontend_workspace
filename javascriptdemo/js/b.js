c4d68b8620fd8c5db62a3af1d470cb17dc12c46b
Hyewon97 <hyewon97@inu.ac.kr>
2022-12-22 12:36:29 +0900

    to js007

---
+++ javascriptdemo/js/b.js
@@ -0,0 +1,7 @@
+// window.onload = function () {
+//   /* 현재 페이지가 파싱이 되면 안의 코드를 실행하라. */
+//   let wrap = document.getElementById("warp");
+//   console.log(wrap.innerText);
+// };
+let wrap = document.getElementById("warp");
+console.log(wrap.innerText);
