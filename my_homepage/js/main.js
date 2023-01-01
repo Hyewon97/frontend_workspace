1f433db2269f1babe3ceed350f5d1feda436e3f3
Hyewon97 <hyewon97@inu.ac.kr>
2023-01-02 00:08:00 +0900

    add login

--- my_homepage/js/main.js
+++ my_homepage/js/main.js
@@ -23,26 +23,6 @@ setInterval(function () {
   }, 600); // 0.6초 동안 실행하라.
 }, 3000);
 
-////////////////////////////
-// 탭변경 변경하기
-///////////////////////////
-function chgTab(i1, i2) {
-  // 탭메뉴                             2개씩 가져오기,배열로
-  let tm = document.querySelectorAll(".tm>li");
-
-  // 탭내용
-  let tc = document.querySelectorAll(".tc>li");
-
-  // 클래스 추가
-  // 0은 공지사항, 인덱스 1은 갤러리가 넘어온다
-  // 0 1 : 공지사항이 추가, 1 0 : 갤러리 추가
-  tm[i1].classList.add("on"); // 클래스 리스트 추가
-  tc[i1].classList.add("on");
-
-  // 클래스 제거
-  tm[i2].classList.remove("on");
-  tc[i2].classList.remove("on");
-}
 /////////////////////////////////////////////////
 // 함수명 : popup
 // 기능 : 팝업 열기/닫기
@@ -51,3 +31,24 @@ function popup(sts) {
   var pop = document.querySelector(".pop");
   pop.style.display = sts;
 }
+
+//////////////////////////////////////////////////////
+// 로그인
+//////////////////////////////////////////////////////
+document.log_f.onsubmit = () => {
+  let idNode = document.querySelector("#userName");
+  let passNode = document.querySelector("#userPassword");
+
+  if (idNode.value == "") {
+    alert("아이디를 입력하세요.");
+    return false;
+  }
+  // elseif 쓰며 안됨. 비교할 대상이 다름.
+  if (passNode.value == "") {
+    alert("비밀번호를 입력하세요.");
+    return false;
+  }
+  // doucument.log_f.action='#'
+  // 강제적으로 submit() 이벤트를 발생시킨다.
+  document.log_f.submit();
+};
